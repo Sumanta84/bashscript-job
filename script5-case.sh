37b84e76bf5112b141eaadb7056787f9361eaca1
u6-devops-wsl-ubuntu24 <u6-devops-wsl-ubuntu24@email.com>
2026-06-25 17:40:07 +0530

    new file and code

--- script5-case.sh
+++ script5-case.sh
@@ -2,16 +2,16 @@
 
 PS3="Select a number for city:"
 select city in "Delhi" "London" "New York" "manchester"; do
-case "$city" in
-    "London" | "manchester") 
-        country="UK";;
-    "Delhi") 
-        country="India";;
-    "New york") 
-        country="US";;
-esac
-echo " The $city in under $country "
-break
+    case "$city" in
+        "London" | "manchester") 
+            country="UK";;
+        "Delhi") 
+            country="India";;
+        "New york") 
+            country="US";;
+    esac
+    echo "The $city in under $country"
+    break
 done
 
 #[w/o ;; it's not working]
